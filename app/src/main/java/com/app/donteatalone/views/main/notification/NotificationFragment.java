8e8ec7ad18e33c804f74b5606506628ca999d312
Tran Thi Hang Nga <hangnga290695@gmail.com>
2017-06-13 20:55:15 +0700

    First update for verrsion2_require_notification

--- app/src/main/java/com/app/donteatalone/views/main/notification/NotificationFragment.java
+++ app/src/main/java/com/app/donteatalone/views/main/notification/NotificationFragment.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,7 +35,6 @@ public class NotificationFragment extends Fragment {
     }
 
     public void init(){
-        Log.e("listInfoNotification","notification");
         rcvInfoNotification=(RecyclerView) viewGroup.findViewById(R.id.fragment_notification_rcv_notification);
         rcvInfoNotification.setLayoutManager(new LinearLayoutManager(getContext()));
     }
